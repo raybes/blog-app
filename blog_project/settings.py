c17750149470500a08682bf772221b1b7a9dbb57
raybes <wrjones311@gmail.com>
2020-06-06 02:24:47 -0700

    More

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = [*]
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
+    'django.contrib.staticfiles',
     'django.contrib.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
